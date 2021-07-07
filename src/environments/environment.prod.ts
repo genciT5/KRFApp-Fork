bf5072d038052c2fb3ec0ad8c064cfb72629befc
Milot <55019464+MilotH2@users.noreply.github.com>
2021-07-07 23:59:07 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 
-const apiLink = 'https://localhost:44322/';
+const apiLink = 'https://krf.azurewebsites.net/';
 
 export const environment = {
   production: true,
   apiBase: `${apiLink}`,
-  apiLink: `${apiLink}api/`
+  apiLink: `${apiLink}api/`,
+  defailtProfilePicture: 'https://lh3.googleusercontent.com/proxy/6X8FY0OwZ3z0rXERgh0SXIn2wan0IklVMutoOXLrUQBCM--C9R0y-ip5YwpuNxeMF9cQe6SIDZnsLC4a03nXPF577oLvjTDqArRy6mbYAbwmPcwaawmptCDf'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 const apiLink = 'https://krf.azurewebsites.net/';
 
 export const environment = {
-  production: false,
+  production: true,
   apiBase: `${apiLink}`,
   apiLink: `${apiLink}api/`,
   defailtProfilePicture: 'https://lh3.googleusercontent.com/proxy/6X8FY0OwZ3z0rXERgh0SXIn2wan0IklVMutoOXLrUQBCM--C9R0y-ip5YwpuNxeMF9cQe6SIDZnsLC4a03nXPF577oLvjTDqArRy6mbYAbwmPcwaawmptCDf'
